68d037ac156616dae765e8c972f4d87aa0a31604
Aiswarya <aiswarya.technical@gmail.com>
2026-05-21 06:43:25 +0530

    fixed render deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+const filePath = path.join(__dirname, "data", "questions.json");
+
 const questions = JSON.parse(
-  fs.readFileSync("./data/questions.json", "utf8")
+  fs.readFileSync(filePath, "utf8")
 );
 
 app.get("/quiz/:topic", (req, res) => {
@@ -17,11 +21,12 @@ app.get("/quiz/:topic", (req, res) => {
   if (questions[topic]) {
     res.json(questions[topic]);
   } else {
-    res.status(404).json({ message: "Topic not found" });
+    res.status(404).json({
+      message: "Topic not found",
+    });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-fetch(` https://online-mcq-quiz-app.onrender.com/${topic}`)
