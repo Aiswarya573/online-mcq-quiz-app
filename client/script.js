3d55dc709e2ab14e5527b178e9565bd308e24290
Aiswarya <aiswarya.technical@gmail.com>
2026-05-21 06:56:54 +0530

    fixed frontend api url

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@ let currentQuestions = [];
 async function loadQuiz() {
     const topic = document.getElementById("topic").value;
 
-    const response = await fetch(`http://localhost:5000/quiz/${topic}`);
+    const response = await fetch(` https://online-mcq-quiz-app.onrender.com${topic}`);
     currentQuestions = await response.json();
 
     const container = document.getElementById("quiz-container");
