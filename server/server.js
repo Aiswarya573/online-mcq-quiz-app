da94b98a5725cacc794df0fb8afe8a8d5d966951
Aiswarya <aiswarya.technical@gmail.com>
2026-05-25 17:52:47 +0530

    fixed backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-const filePath = path.join(__dirname, "data", "questions.json");
+app.get("/", (req, res) => {
+  res.send("MCQ Quiz Backend Running Successfully");
+});
 
 const questions = JSON.parse(
-  fs.readFileSync(filePath, "utf8")
+  fs.readFileSync("./data/questions.json", "utf8")
 );
 
 app.get("/quiz/:topic", (req, res) => {
@@ -21,12 +21,10 @@ app.get("/quiz/:topic", (req, res) => {
   if (questions[topic]) {
     res.json(questions[topic]);
   } else {
-    res.status(404).json({
-      message: "Topic not found",
-    });
+    res.status(404).json({ message: "Topic not found" });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
